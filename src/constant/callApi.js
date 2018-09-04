799d035e98e869572a50ac4ff24ef9ddac24f89d
hieuuet <machouse@Macs-MacBook-Pro.local>
2018-09-04 21:45:52 +0700

    add axios

---
+++ src/constant/callApi.js
@@ -0,0 +1,48 @@
+import { AsyncStorage } from 'react-native';
+import axios from 'axios';
+
+
+export const getRequestApi = async (url) => {
+    // let token = await AsyncStorage.getItem('token');
+    // console.log('token get request', token)
+    let config = {
+        headers: {
+            "Content-Type" : "application/json",
+            // "access-token": token
+        }
+    }
+    let timeRequest = {
+        timeout: 30000
+    }
+
+    return axios.get(url, config, timeRequest).then(res => {
+        const response = res.data;
+
+        return response;
+    }).catch(err => {
+        // return Promise.resolve(_errorOnTryCatch(null, url, err));
+    });
+};
+
+export const postRequestApi = async (url, data, dispatch) => {
+    // let  token = await AsyncStorage.getItem('token') ? await AsyncStorage.getItem('token') : ''
+    // console.log('token', token)
+    let config = {
+        headers: {
+            "Content-Type" : "application/json",
+            // "access-token": token
+        }
+    }
+    let timeRequest = {
+        timeout: 30000
+    }
+
+
+    return axios.post(url, data, config, timeRequest).then(res => {
+        console.log('dataRes post request', res)
+        const response = res.data;
+        return response;
+    }).catch(err => {
+        // return Promise.resolve(_errorOnTryCatch(null, url, err));
+    });
+};
