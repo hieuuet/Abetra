20cf3efbf191dae14bcd461a03ad70be2243b6e8
unknown <anhhieuuet@gmail.com>
2018-09-06 16:42:54 +0700

    trang thai

--- src/components/StatusItems.js
+++ src/components/StatusItems.js
@@ -12,6 +12,7 @@ import {
 
 import moment from 'moment';
 import Icon1 from 'react-native-vector-icons/EvilIcons';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import ReadMore from 'react-native-read-more-text';
 import PhotoGrid from "./PhotoGrid";
 
@@ -22,19 +23,19 @@ class StatusItems extends Component {
 
 
         this.state = {
-            images : [
-                'https://drscdn.500px.org/photo/216465193/m%3D2048_k%3D1_a%3D1/dda61fd7cea5013f8ebe7661b7abea3a',
-                'https://drscdn.500px.org/photo/215467843/m%3D2048_k%3D1_a%3D1/344703e86f31e1fffb2d63effa2cee33',
-                'https://drscdn.500px.org/photo/216340727/m%3D2048_k%3D1_a%3D1/20d583e15467fb39d06d48131767edc2',
-                'https://drscdn.500px.org/photo/215498077/m%3D2048_k%3D1_a%3D1/f79e906eb96938807f6f9d758fc652fd',
-                'https://drscdn.500px.org/photo/216559713/m%3D2048_k%3D1_a%3D1/393ef5251fa94964fe62cad52a416b7e',
-                'https://drscdn.500px.org/photo/214943889/m%3D2048_k%3D1_a%3D1/90bd2e3619dfcaae53fed683561aae1b',
-                'https://drscdn.500px.org/photo/216158509/m%3D2048_k%3D1_a%3D1/cf70d51aab6ca4c4a3c1ecc225c69990',
-                'https://drscdn.500px.org/photo/216111469/m%3D2048_k%3D1_a%3D1/d2d83296c838258095dbf2bffda70602',
-                'https://drscdn.500px.org/photo/216051623/m%3D2048_k%3D1_a%3D1/5a3732bb413f240ad71b8279b038a3ff',
-                'https://drscdn.500px.org/photo/216047335/m%3D2048_k%3D1_a%3D1/4237ac4606474f0ec7ccc05ca311772e',
-                'https://drscdn.500px.org/photo/216000289/m%3D2048_k%3D1_a%3D1/5ac2a21092f9281feef3ab8484d2b19c'
-            ]
+            // images : [
+            //     'https://drscdn.500px.org/photo/216465193/m%3D2048_k%3D1_a%3D1/dda61fd7cea5013f8ebe7661b7abea3a',
+            //     'https://drscdn.500px.org/photo/215467843/m%3D2048_k%3D1_a%3D1/344703e86f31e1fffb2d63effa2cee33',
+            //     'https://drscdn.500px.org/photo/216340727/m%3D2048_k%3D1_a%3D1/20d583e15467fb39d06d48131767edc2',
+            //     'https://drscdn.500px.org/photo/215498077/m%3D2048_k%3D1_a%3D1/f79e906eb96938807f6f9d758fc652fd',
+            //     'https://drscdn.500px.org/photo/216559713/m%3D2048_k%3D1_a%3D1/393ef5251fa94964fe62cad52a416b7e',
+            //     'https://drscdn.500px.org/photo/214943889/m%3D2048_k%3D1_a%3D1/90bd2e3619dfcaae53fed683561aae1b',
+            //     'https://drscdn.500px.org/photo/216158509/m%3D2048_k%3D1_a%3D1/cf70d51aab6ca4c4a3c1ecc225c69990',
+            //     'https://drscdn.500px.org/photo/216111469/m%3D2048_k%3D1_a%3D1/d2d83296c838258095dbf2bffda70602',
+            //     'https://drscdn.500px.org/photo/216051623/m%3D2048_k%3D1_a%3D1/5a3732bb413f240ad71b8279b038a3ff',
+            //     'https://drscdn.500px.org/photo/216047335/m%3D2048_k%3D1_a%3D1/4237ac4606474f0ec7ccc05ca311772e',
+            //     'https://drscdn.500px.org/photo/216000289/m%3D2048_k%3D1_a%3D1/5ac2a21092f9281feef3ab8484d2b19c'
+            // ]
 
 
         }
@@ -105,26 +106,28 @@ class StatusItems extends Component {
                                 onReady={this._handleTextReady}
                                 >
                                 <Text>
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                                    eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                                    enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                                    nisi ut aliquip ex ea commodo consequat.  Duis aute irure dolor
-                                    in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                                    nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-                                    sunt in culpa qui officia deserunt mollit anim id est laborum
+                                    {item.Content}
                                 </Text>
                             </ReadMore>
                         </View>
                     </View>
-                    <PhotoGrid source={this.state.images} />
+                    {
+                        item.images ?  <PhotoGrid source={item.images} /> : null
+                    }
+
                     <View style={{ flexDirection: 'row', marginTop: 20, justifyContent: 'space-between' }}>
                         <View style={{ flexDirection: 'row', marginLeft: 10 }}>
-                            {/* <Icon1 name="like" size={25} color="#424242" /> */}
-                            <Text style={{ color: '#424242' }} > Thích</Text>
+                             <Icon1 name="like" size={25} color="#42A5F5" />
+                            <Text style={{ color: '#42A5F5' }} > 99</Text>
+
+                        </View>
+                        <View style={{ flexDirection: 'row'}}>
+                            <Icon1 name="comment" size={25} color="#42A5F5" />
+                            <Text  style = {{color: "#42A5F5"}}>99</Text>
                         </View>
                         <View style={{ flexDirection: 'row', marginRight: 10 }}>
-                            {/*<Icon1 name="comment" size={25} color="#424242" />*/}
-                            <Text> bình luận</Text>
+                            <Icon name="share-outline" size={25} color="#42A5F5" />
+                            <Text style = {{color: "#42A5F5"}}>99</Text>
                         </View>
 
                     </View>
@@ -142,6 +145,11 @@ class StatusItems extends Component {
                             <Icon1 name="comment" size={25} color="#424242" />
 
                             <Text style={{ color: '#424242' }}>Bình luận</Text>
+                        </View>
+                        <View style={{ flexDirection: 'row', marginRight: 20 }}>
+                            <Icon name="share-outline" size={25} color="#424242" />
+
+                            <Text style={{ color: '#424242' }}>Share</Text>
                         </View>
                     </View>
 
