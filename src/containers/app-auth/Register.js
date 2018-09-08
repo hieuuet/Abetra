c0591b4f75bc847a636fd30fa6e4ae4a0c9c67c9
Kienlv58 <kienlv58@gmail.com>
2018-09-08 10:41:35 +0700

    login fb view

--- src/containers/app-auth/Register.js
+++ src/containers/app-auth/Register.js
@@ -18,6 +18,12 @@ import { connect } from "react-redux";
 import { postRegister } from "../../actions/registerActions";
 import CheckBox from "../../components/CheckBox ";
 import { ButtonBorder, ViewLoading } from "../../components/CommonView";
+import {
+  LoginManager,
+  AccessToken,
+  GraphRequest,
+  GraphRequestManager
+} from "react-native-fbsdk";
 
 class Register extends Component {
   constructor(props) {
@@ -72,6 +78,84 @@ class Register extends Component {
         [{ text: "OK", onPress: () => console.log("OK Pressed") }],
         { cancelable: false }
       );
+    }
+  };
+
+  /**
+   * Login with FB
+   */
+  facebookLogin = async () => {
+    // native_only config will fail in the case that the user has
+    // not installed in his device the Facebook app. In this case we
+    // need to go for webview.
+    let result;
+    try {
+      LoginManager.setLoginBehavior("NATIVE_ONLY");
+      result = await LoginManager.logInWithReadPermissions([
+        "public_profile",
+        "email"
+      ]);
+    } catch (nativeError) {
+      try {
+        LoginManager.setLoginBehavior("WEB_ONLY");
+        result = await LoginManager.logInWithReadPermissions([
+          "public_profile",
+          "email"
+        ]);
+      } catch (webError) {
+        console.log("web", webError);
+        // show error message to the user if none of the FB screens
+        Alert.alert("Thông báo", "Lỗi đăng nhập", [{ text: "OK" }], {
+          cancelable: false
+        });
+      }
+    }
+    // handle the case that users clicks cancel button in Login view
+    if (result.isCancelled) {
+    } else {
+      // Create a graph request asking for user information
+      this.fbGraphRequest(
+        "email,name,first_name,middle_name,last_name,picture.type(large)",
+        this.fbLoginCallback
+      );
+    }
+  };
+  fbGraphRequest = async (fields, callback) => {
+    const accessData = await AccessToken.getCurrentAccessToken();
+    // Create a graph request asking for user information
+    const infoRequest = new GraphRequest(
+      "/me",
+      {
+        accessToken: accessData.accessToken,
+        parameters: {
+          fields: {
+            string: fields
+          }
+        }
+      },
+      callback
+    );
+    // Execute the graph request created above
+    new GraphRequestManager().addRequest(infoRequest).start();
+  };
+  fbLoginCallback = (error, result) => {
+    if (error) {
+      Alert.alert(
+        "Thông báo",
+        "Không lấy được thông tin facebook",
+        [{ text: "OK" }],
+        { cancelable: false }
+      );
+    } else {
+      this.dataUser = {
+        ...this.dataUser,
+        fullName: result.name,
+        urlImg: result.picture.data.url,
+        email: result.email,
+        id: result.id
+      };
+      console.log("result", result);
+      //Call api login fb
     }
   };
 
@@ -133,7 +217,7 @@ class Register extends Component {
             />
             <View style={styles.view_login}>
               <Text>Đăng nhập bằng Facebook</Text>
-              <TouchableOpacity>
+              <TouchableOpacity onPress={this.facebookLogin}>
                 <Image
                   style={styles.img_fb}
                   resizeMode="cover"
