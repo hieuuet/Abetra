668e7c126e409a571744941a2c43f08c881a499e
mac mini <macmini@macs-Mac-mini.local>
2019-01-05 16:50:21 +0700

    fix and build ios

--- src/constant/assets.js
+++ src/constant/assets.js
@@ -1,6 +1,6 @@
 export const IMAGE = {
   logo: require("../../assets/logo.png"),
-  logo_white: require("../../assets/logo-mới.png"),
+  logo_white: require("../../assets/logo_new.png"),
   logo_fb: require("../../assets/logo_fb.png"),
   imag_icon: require("../../assets/image_icon.png"),
   hashtag_icon: require("../../assets/hashtag.png"),
